b1ec2108e3dca7a1a7a4cee4a4a68c8964709354
soyUnaGata <fallen.snitch@gmail.com>
2025-09-01 13:44:33 +0300

    install and implement factory+fixtures

---
+++ src/Factory/MistyTreasureFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\MistyTreasure;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<MistyTreasure>
+ */
+final class MistyTreasureFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return MistyTreasure::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'description' => self::faker()->text(),
+            'name' => self::faker()->text(255),
+            'value' => self::faker()->randomNumber(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(MistyTreasure $mistyTreasure): void {})
+        ;
+    }
+}
